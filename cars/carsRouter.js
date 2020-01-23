5b46409f6bc64f2736aa408e5ecf431ddc086fb2
imxande <alerodriguezlarosa@gmail.com>
2020-01-23 07:56:45 -0500

    created cars data base

--- cars/carsRouter.js
+++ cars/carsRouter.js
@@ -1,6 +1,21 @@
 // importing express
 const express = require("express");
 
+// importing knex
+const knex = require("knex");
+
+// knex configuration
+const knexConfiguration = {
+    client: "sqlite3",
+    connection: {
+        filename: "./data/cars.db3"
+    },
+    useNullAsDefault: true
+};
+
+// i need a connection to the data base
+const carsDB = knex(knexConfiguration);
+
 // using express to create a router
 const router = express.Router();
 
