097969d68f4db94b53ff86e67f1c77dcef2ad355
imxande <alerodriguezlarosa@gmail.com>
2020-01-23 08:09:41 -0500

    getting cars by its unique id

--- cars/carsRouter.js
+++ cars/carsRouter.js
@@ -38,6 +38,22 @@ router.get("/", (req, res) => {
         });
 });
 
+// getting car by id
+router.get("/:id", (req, res) => {
+    const { id } = req.params;
+    // select * from cars where id = 2
+    carsDB("cars")
+        // .where({ id: id })
+        .where("id", "=", id)
+        .first()
+        .then(response => {
+            res.json(response);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({ message: "Failed to retrieve cars with specified ID" });
+        });
+});
 
 // exporting the router
 module.exports = router;
